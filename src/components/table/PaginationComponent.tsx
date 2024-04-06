744d6dca9769d4c3c949f3a030bcc8173d2a1d7a
Willyan Costa <willyancr@gmail.com>
2024-04-06 16:43:30 -0300

    Add pagination functionality to PaginationComponent
    Integrate pagination logic and controls to navigate through attendee data.

--- src/components/table/PaginationComponent.tsx
+++ src/components/table/PaginationComponent.tsx
@@ -7,27 +7,76 @@ import {
   PaginationContent,
   Pagination,
 } from '@/components/ui/pagination';
+import { attendees } from '../dataUsers';
 
-function PaginationComponent() {
+type PaginationProps = {
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function PaginationComponent({ currentPage, setCurrentPage }: PaginationProps) {
+  const qtaPages = Math.ceil(attendees.length / 5);
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(attendees.length / qtaPages); i++) {
+    pages.push(i);
+  }
+
+  function handleClickPrevious() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+  function handleClickNext() {
+    if (currentPage + 1 <= qtaPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+  function handleClickLast() {
+    setCurrentPage(qtaPages);
+  }
+
   return (
     <div className="flex items-center gap-4 px-4 py-2 w-full">
-      <div>Mostrando 1 de 100</div>
+      <div>Mostrando 10 de {attendees.length}</div>
       <div className="ml-auto">
         <Pagination>
           <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" onClick={handleClickPrevious} />
+            </PaginationItem>
+            {pages.map((page, index) => (
+              <PaginationItem
+                key={index}
+                className={
+                  currentPage === page
+                    ? 'bg-neutral-100 text-black font-bold rounded-md'
+                    : ''
+                }
+              >
+                <PaginationLink href="#" onClick={() => setCurrentPage(page)}>
+                  {page}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
 
-            <PaginationPrevious href="#" />
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem
+              className={
+                currentPage === qtaPages
+                  ? 'bg-neutral-100 text-black font-bold rounded-md'
+                  : ''
+              }
+            >
+              <PaginationLink href="#" onClick={handleClickLast}>
+                {qtaPages}
+              </PaginationLink>
+            </PaginationItem>
 
-            <PaginationLink href="#" isActive>1</PaginationLink>
-            <PaginationLink href="#">2</PaginationLink>
-            <PaginationLink href="#">3</PaginationLink>
-
-            <PaginationEllipsis />
-
-            <PaginationLink href="#">10</PaginationLink>
-
-            <PaginationNext href="#" />
-
+            <PaginationItem>
+              <PaginationNext href="#" onClick={handleClickNext} />
+            </PaginationItem>
           </PaginationContent>
         </Pagination>
       </div>
