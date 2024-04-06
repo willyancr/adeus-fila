691ac2a78922d13e71e75975ba503dbd07b9c769
Willyan Costa <willyancr@gmail.com>
2024-04-06 16:43:36 -0300

    Update number of attendees to 212
    Changed the number of attendees in the dataUsers component from 100 to 212 for more accurate representation.

--- src/components/dataUsers.tsx
+++ src/components/dataUsers.tsx
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-export const attendees = Array.from({ length: 100 }).map(() => {
+export const attendees = Array.from({ length: 212 }).map(() => {
   return {
     id: faker.number.int({ min: 1000, max: 2000 }),
     name: faker.person.fullName(),
