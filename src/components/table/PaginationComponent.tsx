9711ecc523b6b20b339db9bed01dbbef849123d8
Willyan Costa <willyancr@gmail.com>
2024-04-09 15:27:01 -0300

    feat: Refactor PaginationComponent to use total prop
    Update PaginationComponent to use the total prop instead of attendees.length for
    calculations and display. Also, refactor setCurrentPage function to update URL
    and browser history.

--- src/components/table/PaginationComponent.tsx
+++ src/components/table/PaginationComponent.tsx
@@ -7,28 +7,44 @@ import {
   PaginationContent,
   Pagination,
 } from '@/components/ui/pagination';
-import { attendees } from '../dataUsers';
+import { AttendeesProps } from './Table';
 
 type PaginationProps = {
-  currentPage: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  total: number;
+  attendees: AttendeesProps[];
 };
 
-function PaginationComponent({ currentPage, setCurrentPage }: PaginationProps) {
-  const qtaPages = Math.ceil(attendees.length / 5);
+function PaginationComponent({
+  page,
+  setPage,
+  total,
+  attendees,
+}: PaginationProps) {
+  const qtaPages = Math.ceil(total / 10);
   const pages = [];
-  for (let i = 1; i <= Math.ceil(attendees.length / qtaPages); i++) {
+  for (let i = 1; i <= qtaPages; i++) {
     pages.push(i);
   }
 
+  //Define a página atual na URL e atualiza o histórico do navegador.
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+    url.searchParams.set('page', String(page));
+    window.history.pushState({}, '', url);
+
+    setPage(page)
+  }
+
   function handleClickPrevious() {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
+    if (page > 1) {
+      setCurrentPage(page - 1);
     }
   }
   function handleClickNext() {
-    if (currentPage + 1 <= qtaPages) {
-      setCurrentPage(currentPage + 1);
+    if (page + 1 <= qtaPages) {
+      setCurrentPage(page + 1);
     }
   }
   function handleClickLast() {
@@ -37,24 +53,26 @@ function PaginationComponent({ currentPage, setCurrentPage }: PaginationProps) {
 
   return (
     <div className="flex items-center gap-4 px-4 py-2 w-full">
-      <div>Mostrando 10 de {attendees.length}</div>
+      <div>
+        Mostrando {attendees.length} de {total}
+      </div>
       <div className="ml-auto">
         <Pagination>
           <PaginationContent>
             <PaginationItem>
               <PaginationPrevious href="#" onClick={handleClickPrevious} />
             </PaginationItem>
-            {pages.map((page, index) => (
+            {pages.map((item, index) => (
               <PaginationItem
                 key={index}
                 className={
-                  currentPage === page
+                  page === item
                     ? 'bg-neutral-100 text-black font-bold rounded-md'
                     : ''
                 }
               >
-                <PaginationLink href="#" onClick={() => setCurrentPage(page)}>
-                  {page}
+                <PaginationLink href="#" onClick={() => setPage(item)}>
+                  {item}
                 </PaginationLink>
               </PaginationItem>
             ))}
@@ -64,7 +82,7 @@ function PaginationComponent({ currentPage, setCurrentPage }: PaginationProps) {
             </PaginationItem>
             <PaginationItem
               className={
-                currentPage === qtaPages
+                page === qtaPages
                   ? 'bg-neutral-100 text-black font-bold rounded-md'
                   : ''
               }
