e73e88d8aa7b0a2f74876b8d112db68fcc5ef6f3
Willyan Costa <willyancr@gmail.com>
2024-04-09 15:26:29 -0300

    feat: Update search functionality in AttendeeList component
    Refactor search functionality to store and update search query in URL
    parameters and browser history for better user experience. Add
    setCurrentSearch function to handle updating search query. Use
    onSearchInput function to handle input changes.

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import searchSVG from '../assets/search.svg';
 import TableComponent from './table/Table';
 import { Input } from './ui/input';
 
 function AttendeeList() {
-  const [search, setSearch] = React.useState('');
+  const [search, setSearch] = React.useState(() => {
+    const url = new URL(window.location.toString());
+
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? '';
+    }
+    return '';
+  });
+
+  //Atualiza a query de busca atual na URL e no estado do histórico do navegador.
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString()); // Cria uma nova instância de URL com a URL atual do navegador
+    url.searchParams.set('search', search);
+    window.history.pushState({}, '', url);
+
+    setSearch(search);
+  }
+
+  function onSearchInput(event: ChangeEvent<HTMLInputElement>) {
+    setCurrentSearch(event.target.value);
+  }
+
   return (
     <div>
       <div className="flex items-center w-[437px] gap-4 mb-4">
@@ -19,7 +40,7 @@ function AttendeeList() {
             placeholder="Buscar participante..."
             className="dark bg-zinc-900 border border-white/10 text-sm w-72 px-9 focus-visible:outline-none"
             value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={onSearchInput}
           />
         </div>
       </div>
